1a55ca120a7b2c3ca0bd2bae04e0bf9a600ade01
Maxime Pigeon <me@maximepigeon.com>
2025-09-23 15:38:41 -0400

    Simple forms, part 2

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Question
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+from .models import Choice, Question
 
 
 def index(request):
@@ -11,7 +14,7 @@ def index(request):
 
 def detail(request, question_id):
     context = {
-        "question": Question.objects.get(pk=question_id),
+        "question": get_object_or_404(Question, pk=question_id),
         "error_message": "",
     }
     return render(request, "polls/detail.html", context)
@@ -24,4 +27,20 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        choice_id = request.POST["choice"]
+        selected_choice = question.choice_set.get(pk=choice_id)
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("results", args=(question.id,)))
